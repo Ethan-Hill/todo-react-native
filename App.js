5e38813d746a106517cd69abb0261da43cf10e0b
Ethan Hill <ethanhilljay@outlook.com>
2021-06-04 23:03:07 +0100

    feat: Created index page

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import Task from "./components/Task"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.textWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks go */}
+          <Task text="Task 1" />
+          <Task text="Task 2" />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e1e1e1",
   },
-});
+  textWrapper: { paddingTop: 80, paddingHorizontal: 20 },
+  sectionTitle: { fontSize: 24, fontWeight: "bold" },
+  items: { margin: 30 },
+})
